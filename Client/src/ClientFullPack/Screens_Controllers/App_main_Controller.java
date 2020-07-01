1b68a0d7f66275268b379f99f2e691f4ba518c1f
iurii <babalin2011@gmail.com>
2020-07-02 02:35:36 +0300

    Я закончился

---
+++ Client/src/ClientFullPack/Screens_Controllers/App_main_Controller.java
@@ -0,0 +1,28 @@
+package ClientFullPack.Screens_Controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableView;
+
+public class App_main_Controller {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ComboBox<?> Languages;
+
+    @FXML
+    private TableView<?> objectTable;
+
+    @FXML
+    void initialize() {
+
+
+    }
+}
