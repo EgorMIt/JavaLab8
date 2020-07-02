5bb77014e0d4d352346d1e68bed4576cc23f7dc6
EgorMIt <egor.mitrofanov@hotmail.com>
2020-07-02 22:47:44 +0300

    ы

--- Client/src/ClientFullPack/Screens_Controllers/App_main_Controller.java
+++ Client/src/ClientFullPack/Screens_Controllers/App_main_Controller.java
@@ -6,7 +6,9 @@ import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
 public class App_main_Controller {
@@ -22,6 +24,57 @@ public class App_main_Controller {
 
     @FXML
     private TableView<?> objectTable;
+
+    @FXML
+    private TableColumn<?, ?> ID;
+
+    @FXML
+    private TableColumn<?, ?> Owner;
+
+    @FXML
+    private TableColumn<?, ?> Name;
+
+    @FXML
+    private TableColumn<?, ?> X;
+
+    @FXML
+    private TableColumn<?, ?> Y;
+
+    @FXML
+    private TableColumn<?, ?> CreationDate;
+
+    @FXML
+    private TableColumn<?, ?> Area;
+
+    @FXML
+    private TableColumn<?, ?> Population;
+
+    @FXML
+    private TableColumn<?, ?> MetersAboveSeaLevel;
+
+    @FXML
+    private TableColumn<?, ?> Climate;
+
+    @FXML
+    private TableColumn<?, ?> Government;
+
+    @FXML
+    private TableColumn<?, ?> StandartOfLiving;
+
+    @FXML
+    private TableColumn<?, ?> Governor;
+
+    @FXML
+    private TableColumn<?, ?> Age;
+
+    @FXML
+    private TableColumn<?, ?> Birthday;
+
+    @FXML
+    private Button Add_button;
+
+    @FXML
+    private Button Clear_button;
 
     @FXML
     void initialize() {
